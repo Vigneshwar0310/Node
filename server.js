c026c10be405fa6985928e1e8a99cf1268f0e951
Vigneshwar0310 <vigneshwardurairaj3@gmail.com>
2025-03-24 14:40:41 +0530

    second

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
-const path = require('path');
+const path = require("path");
 
-const fspromises = require('fs').promises;
+const fspromises = require("fs").promises;
 
 const fileoperations = async () => {
-    try{
-        // Reading a file
-        const data = await fspromises.readFile(path.join(__dirname,'Files','hello.txt'), 'utf8');         
-        console.log(data);
+  try {
+    // Reading a file
+    const data = await fspromises.readFile(
+      path.join(__dirname, "Files", "hello.txt"),
+      "utf8"
+    );
+    console.log(data);
 
-        // Writing a file
-        await fspromises.writeFile(path.join(__dirname,'Files','world.txt') , 'Hello World!');
-        console.log('File created!');
+    // Writing a file
+    await fspromises.writeFile(
+      path.join(__dirname, "Files", "world.txt"),
+      "Hello World!"
+    );
+    console.log("File created!");
 
-        // Appending to a file
-        await fspromises.appendFile(path.join(__dirname,'Files','world.txt') , '\n I am learning Node.js'); 
-        console.log('File updated!');
+    // Appending to a file
+    await fspromises.appendFile(
+      path.join(__dirname, "Files", "world.txt"),
+      "\n I am learning Node.js"
+    );
+    console.log("File updated!");
 
-        // Renaming a file
-        await fspromises.rename(path.join(__dirname,'Files','world.txt'),path.join(__dirname,'Files','msg.txt'));
-        console.log('File renamed!');
+    // Renaming a file
+    await fspromises.rename(
+      path.join(__dirname, "Files", "world.txt"),
+      path.join(__dirname, "Files", "msg.txt")
+    );
+    console.log("File renamed!");
 
-        // Deleting a file
-        await fspromises.unlink(path.join(__dirname,'Files','msg.txt'));
-        console.log('File deleted!');
-    }
-    catch(err){
-        console.error(err);
-    }
-}
+    // Deleting a file
+    await fspromises.unlink(path.join(__dirname, "Files", "msg.txt"));
+    console.log("File deleted!");
+  } catch (err) {
+    console.error(err);
+  }
+};
 fileoperations();
 
-
-process.on('uncaughtException', (err) => {
-  console.log('There was an Uncaught: ' + err);
+process.on("uncaughtException", (err) => {
+  console.log("There was an Uncaught: " + err);
   process.exit(1);
 });
